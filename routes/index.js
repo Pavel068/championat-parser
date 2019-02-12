93dc63bd551c7ebc5f35f77e42fe465f4c0af6b8
Pavel <Pavel>
2019-02-12 08:22:19 +0300

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,38 @@ var router = express.Router();
 
 var Parser = require('../lib/parser');
 
-router.get('/', function(req, res, next) {
-  let parser = new Parser('football', '2019-02-10');
-  parser.parse()
-      .then((response) => {
-        res.render('index', response);
-      })
-      .catch((error) => {
-        res.render('index', error);
-      });
+router.get('/:sport/:date', function (req, res, next) {
+    let parser = new Parser(req.params.sport, req.params.date);
+    parser.parse()
+        .then((response) => {
+            res.render('index', response);
+        })
+        .catch((error) => {
+            res.render('index', error);
+        });
 });
+
+router.get('/:sport/', function (req, res, next) {
+    let parser = new Parser(req.params.sport, '2019-02-11');
+    parser.parse()
+        .then((response) => {
+            res.render('index', response);
+        })
+        .catch((error) => {
+            res.render('index', error);
+        });
+});
+
+router.get('/', function (req, res, next) {
+    let parser = new Parser('football', '2019-02-11');
+    parser.parse()
+        .then((response) => {
+            res.render('index', response);
+        })
+        .catch((error) => {
+            res.render('index', error);
+        });
+});
+
 
 module.exports = router;
